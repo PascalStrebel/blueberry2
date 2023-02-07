90e7961e6ccb5cedd57c2b5a84e87cda03bfed0e
Zadrian <adrian.brunner@gmail.com>
2023-02-07 15:54:08 +0100

    backend.ts auskommentiert

--- frontend/src/api/backend.ts
+++ frontend/src/api/backend.ts
@@ -1,12 +1,14 @@
-import {Child} from "@/model/model";
-import axios from "axios";
+// import { Child } from '@/model/model';
+// import axios from 'axios';
 
-export async function getChildren(): Promise<Child[]> {
-    let childrenResponse = await axios.get('http://localhost:8000/api/children');
-    return childrenResponse.data._embedded.children;
-}
+// export async function getChildren(): Promise<Child[]> {
+//   let childrenResponse = await axios.get('http://localhost:8080/api/children');
+//   return childrenResponse.data._embedded.children;
+// }
 
-export async function getChildById(id: number): Promise<Child> {
-    let childrenResponse = await axios.get('http://localhost:8000/api/children/' + id);
-    return childrenResponse.data;
-}
+// export async function getChildById(id: number): Promise<Child> {
+//   let childrenResponse = await axios.get(
+//     'http://localhost:8080/api/children/' + id
+//   );
+//   return childrenResponse.data;
+// }
