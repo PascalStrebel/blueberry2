5c8da887a9ae82c013d53f279c509c84a00c74e2
Pascal Strebel <strebpas@students.zhaw.ch>
2023-02-08 16:07:27 +0100

    disables completed buttons

--- frontend/src/views/ChildDetail.vue
+++ frontend/src/views/ChildDetail.vue
@@ -1,33 +1,30 @@
 <template>
   <ion-page>
     <div>
-      <PageDefaultHeader myTitle="My Page Title" />
+      <PageDefaultHeader myTitle="My Page Title"/>
     </div>
     <ion-card>
       <ion-card-header>
         <ion-card-title>{{ child.firstName }} {{ child.name }}</ion-card-title>
         <ion-card-subtitle>Birthday: {{ child.birthdate }}</ion-card-subtitle>
         <ion-card-subtitle
-          >Nationality: {{ child.nationality }}</ion-card-subtitle
+        >Nationality: {{ child.nationality }}
+        </ion-card-subtitle
         >
         <ion-card-subtitle>Entry Date: {{ child.entryDate }}</ion-card-subtitle>
       </ion-card-header>
     </ion-card>
 
-
     <ion-accordion-group>
-      <ion-accordion v-for="category in categories" :key="category" >
+      <ion-accordion v-for="category in categories" :key="category">
         <ion-item slot="header" color="light">
-          <ion-label>{{` ${category} `}}</ion-label>
+          <ion-label>{{ ` ${category} (${getChildObservationPercent(category)})` }}</ion-label>
         </ion-item>
 
-        <ion-item class="ion-padding" slot="content">
-          <ion-label position="stacked">{{ getChildObservationPercent(category) }}</ion-label>
-        </ion-item>
-
-        <ion-item v-for="observation in observations.filter(obs => obs.category === category)" class="ion-padding" slot="content">
+        <ion-item v-for="observation in observations.filter(obs => obs.category === category)" class="ion-padding"
+                  slot="content">
           <ion-label position="stacked">{{ observation.text }}</ion-label>
-          <ion-button @click="completeObservation(child, observation)">Complete</ion-button>
+          <ion-button @click="completeObservation(child, observation)" :disabled="observationCompleted(observation)">Complete</ion-button>
         </ion-item>
 
       </ion-accordion>
@@ -37,10 +34,10 @@
 
 <script setup lang="ts">
 import PageDefaultHeader from '../components/PageDefaultHeader.vue';
-import { onMounted } from '@vue/runtime-core';
+import {onMounted} from '@vue/runtime-core';
 import {createChildObservation, getChildById, getChildObservationsById, getObservations} from '@/api/backend';
 import {Child, ChildObservation, Observation} from '@/model/model';
-import { defineComponent } from 'vue';
+import {defineComponent} from 'vue';
 import {
   IonCard,
   IonCardContent,
@@ -55,8 +52,8 @@ import {
   IonItem,
   IonLabel,
 } from '@ionic/vue';
-import { ref } from 'vue';
-import { useRoute } from 'vue-router';
+import {ref} from 'vue';
+import {useRoute} from 'vue-router';
 
 let child = ref<Child>({} as Child);
 onMounted(async () => {
@@ -73,25 +70,29 @@ let childObservations = ref<ChildObservation[]>([]);
 onMounted(async () => {
   observations.value = await getObservations();
   let categoryStrings = observations.value.map(
-    (observation) => observation.category
+      (observation) => observation.category
   );
   categoryStrings = categoryStrings.filter(
-    (n, i) => categoryStrings.indexOf(n) === i
+      (n, i) => categoryStrings.indexOf(n) === i
   );
   categories.value = categoryStrings;
   childObservations.value = await getChildObservationsById(+id);
 });
 
 async function completeObservation(child: Child, observation: Observation) {
-  createChildObservation(child, observation);
-  childObservations.value = await getChildObservationsById(+id)
+  await createChildObservation(child, observation);
+  window.location.reload();
 }
 
 function getChildObservationPercent(category: string): string {
   let relevantObservations = observations.value.filter(obs => obs.category === category).length;
   let relevantChildObservations = childObservations.value.filter(co => co?.observation.category === category).length;
 
-return (relevantChildObservations / relevantObservations) * 100 + '%';
+  return (relevantChildObservations / relevantObservations) * 100 + '%';
+}
+
+function observationCompleted(observation: Observation): boolean {
+  return childObservations.value.filter(co => co?.observation.id === observation.id && co.child.id == child.value.id).length > 0;
 }
 </script>
 
